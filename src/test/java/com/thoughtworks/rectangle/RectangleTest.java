d2f080066a98cb9d3158b749f45723a75da1e77e
Piyush Chhawachharia <pichhawachharia891@gmail.com>
2024-06-04 18:11:15 +0000

    Refactor. Changed test method name to follow Should..When convention

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class RectangleTest{
     
     @Test
-    void shouldReturnOneWhenLengthIsOneAndBreadthIsOne(){
+    void shouldReturnAreaOfOneWhenLengthIsOneAndBreadthIsOne(){
         Rectangle rectangle=new Rectangle(1,1);
 
         int expected=1;
